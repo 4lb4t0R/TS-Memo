87cbbe12cafb882b6ea14fa05c64ecad4b0d7a2f
4lb4t0R <h4p4st4n7ir@gmail.com>
2022-12-28 11:12:43 +0100

    ++

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,6 @@
-// Init projet sous npm, npm install typescript, créa fichier .ts, npx tsc "nomdufichier" > traduira TS en JS
-"use Strict"; //à placer en début de code, permet meilleur check et correction d'erreurs
+// Init projet sous npm, npm install typescript, créa fichier .ts, npx tsc "nomdufichier" > traduira TS en JS, npx tsc --watch pour compiler en live
+"use Strict";
+//à placer en début de code, permet meilleur check et correction d'erreurs
 console.log("Yo bg");
 // Typage de TS
 const a = "yo"; //tag en tant que chaîne de caractères
@@ -9,20 +10,52 @@ const d = null; //tag en tant que valeur nulle
 const arr = ["yo", "yo", "yo"]; //tag en tant que tableau composé de chaînes de caractères
 const tabl = ["yo", 3]; //tag en tant que fourre-tout, à éviter mais utile quand mix de caractères à définir
 const user = {
-  firstname: "Julian",
-  lastname: "Assange",
+    firstname: "Julian",
+    lastname: "Assange",
 }; //définit valeur attendues dans un objet ; ajout du ? rend optionnel, ex: lastname?:string sera optionnel, user pourra fonctionner sans
 // [key:string] : string au lieu de lastname signifie qu'on attend une clé de caractères contenant des caractères, est dynamique
 const date = new Date(); //définit format spécifique et permet de le décla direct
-const cb = (e) => {};
+const cb = (e) => { };
 //:void sert à dire qu'on s'en blc du retour de la fonction, si jamais contient un return, sera visible mais non exploitable
 const bouton = document.querySelector("test"); //permet de définir le type d'élément ciblé en rajoutant as + sélection de l'élément HTML
 const mdp = { motdepasse: "julien34" }; //permet de dire qu'on attend des nombres et/ou une chaîne de caractères
 // Narrowing
 function printId(id) {
-  if (typeof id === "number") {
-    console.log((id * 2).toString());
-  } else {
-    console.log(id.toUpperCase());
-  }
+    if (typeof id === "number") {
+        console.log((id * 2).toString());
+    }
+    else {
+        console.log(id.toUpperCase());
+    }
 }
+const usheure = { firstname: "Wolfgang", lastname: "VonGoethe" }; //Crée un raccourci, similaire à  un mixin en CSS
+//Generic
+function identity(arg) {
+    return arg;
+}
+const aa = identity(3); //sert à paramétrer et affiner grâce aux <> qui déterminent ce que l'on rentre et ce que l'on attend, ce que ça doit être
+function consoleSize(arg) {
+    console.log(arg.length);
+    return arg;
+}
+const abb = consoleSize(["3", 2]); //sans extends length, renverrait une erreur, permet de forcer et assimiler type
+// readonly devant une propriété la rend inmodifiable par une erreur JS
+//Les Classes
+class A {
+    constructor() {
+        this.a = 3; //rend propriété de la classe uniquement accessible dans la classe; #a rend privée ++, remplace private
+        this.b = 4; //rend propriété uniquement accessible dans la classe + aux childrens
+        this.c = 5; //rend propriété accessible pour tlm depuis n'importe où, est par défaut
+    }
+}
+const x = ["fraise", 3]; //avec type, définit longueur+contenu du tableau, puis on le remplit, le rend immuable et h24 défini
+//Enum
+var STEPS;
+(function (STEPS) {
+    STEPS["Intro"] = "Intro";
+    STEPS["Developpement"] = "Developpement";
+    STEPS["Conclusion"] = "Conclusion";
+    STEPS["Ouverture"] = "Ouverture";
+})(STEPS || (STEPS = {}));
+const step = STEPS.Developpement;
+export {};
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
-// Init projet sous npm, npm install typescript, créa fichier .ts, npx tsc "nomdufichier" > traduira TS en JS
-"use Strict"; //à placer en début de code, permet meilleur check et correction d'erreurs
+// Init projet sous npm, npm install typescript, créa fichier .ts, npx tsc "nomdufichier" > traduira TS en JS, npx tsc --watch pour compiler en live
+"use Strict";
+import { ListFormat } from "../node_modules/typescript/lib/typescript";
+
+//à placer en début de code, permet meilleur check et correction d'erreurs
 console.log("Yo bg");
 
 // Typage de TS
@@ -33,3 +36,49 @@ function printId(id: number | string) {
 //permet d'utiliser des if else et trigger différentes réactions selon différentes saisies avec typeof instanceof
 
 // Alias et Generic
+
+//Alias
+
+type User = { firstname: string; lastname: string };
+const usheure: User = { firstname: "Wolfgang", lastname: "VonGoethe" }; //Crée un raccourci, similaire à  un mixin en CSS
+
+//Generic
+
+function identity<ArgType>(arg: ArgType): ArgType {
+  return arg;
+}
+const aa = identity<number>(3); //sert à paramétrer et affiner grâce aux <> qui déterminent ce que l'on rentre et ce que l'on attend, ce que ça doit être
+
+function consoleSize<Type extends { length: number }>(arg: Type): Type {
+  console.log(arg.length);
+  return arg;
+}
+const abb = consoleSize(["3", 2]); //sans extends length, renverrait une erreur, permet de forcer et assimiler type
+
+// readonly devant une propriété la rend inmodifiable par une erreur JS
+
+//Les Classes
+
+class A {
+  private a = 3; //rend propriété de la classe uniquement accessible dans la classe; #a rend privée ++, remplace private
+  protected b = 4; //rend propriété uniquement accessible dans la classe + aux childrens
+  public c = 5; //rend propriété accessible pour tlm depuis n'importe où, est par défaut
+}
+//à coupler avec this pour devenir un crack
+
+//Tuples
+
+// est un tableau défini immuable
+
+type ListItems = [string, number];
+const x: ListItems = ["fraise", 3]; //avec type, définit longueur+contenu du tableau, puis on le remplit, le rend immuable et h24 défini
+
+//Enum
+
+enum STEPS {
+  Intro = "Intro",
+  Developpement = "Developpement",
+  Conclusion = "Conclusion",
+  Ouverture = "Ouverture",
+}
+const step: STEPS = STEPS.Developpement;
